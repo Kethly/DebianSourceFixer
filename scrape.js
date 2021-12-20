7ae71f9a7a68b887fe19533e35bccf39a80ff68c
Kethly <57457270+Kethly@users.noreply.github.com>
2021-12-20 17:51:34 -0500

    Update scrape.js

--- scrape.js
+++ scrape.js
@@ -1,25 +1,26 @@
 function GetData()  {
-  fetch("https://www.debian.org/distrib/packages", {Access-Control-Allow-Origin: '*'})
-  .then(response => {
-    // indicates whether the response is successful (status code 200-299) or not
-    if (!response.ok) {
-      throw new Error(`Request failed with status ${reponse.status}`)
-    }
-    return response
-//   var XMLReq = new XMLHttpRequest();
-//   //header('Access-Control-Allow-Origin: *');
-//   XMLReq.open("GET", "https://www.debian.org/favicon.ico");
-  
-//   XMLReq.setRequestHeader('Access-Control-Allow-Origin', 'https://www.debian.org/favicon.ico');
-  
-
-//   XMLReq.onreadystatechange = function() {
-//     console.log(XMLReq.responseText);
-//     if(XMLReq.readyState == 4 && XMLReq.status == 200) {
-//       console.log(XMLReq.responseText);
+//   fetch("https://www.debian.org/distrib/packages", {Access-Control-Allow-Origin: '*'})
+//   .then(response => {
+//     // indicates whether the response is successful (status code 200-299) or not
+//     if (!response.ok) {
+//       throw new Error(`Request failed with status ${reponse.status}`)
 //     }
-//   }
+//     return response
+  var XMLReq = new XMLHttpRequest();
+  //header('Access-Control-Allow-Origin: *');
+  XMLReq.open("GET", "https://www.debian.org/favicon.ico");
+  
+  XMLReq.setRequestHeader('Access-Control-Allow-Origin', 'https://www.debian.org/favicon.ico');
+  
 
-//   XMLReq.send();
+  XMLReq.onreadystatechange = function() {
+    XMLReq.response.set('Access-Control-Allow-Origin', '*');
+    console.log(XMLReq.responseText);
+    if(XMLReq.readyState == 4 && XMLReq.status == 200) {
+      console.log(XMLReq.responseText);
+    }
+  }
+
+  XMLReq.send();
 }
 
