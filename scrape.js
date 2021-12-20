ef5e844c3c8a04894ddb1ad530999ca61b06a5da
Kethly <57457270+Kethly@users.noreply.github.com>
2021-12-20 15:34:32 -0500

    Update scrape.js

--- scrape.js
+++ scrape.js
@@ -2,12 +2,12 @@ define(["request-promise", "cheerio"],
     function(request, cheerio) {
 //       const request = require("request-promise");
 //       const cheerio = require("cheerio");
-      function getWebsite(){
+
         request("https://www.bullion-rates.com/gold/INR/2007-1-history.htm", (error, response, html) => {
                 if(!error && response.statusCode==200) {
                       const $= cheerio.load(html);
                         }
                 });
-      }
+      
 });
 
