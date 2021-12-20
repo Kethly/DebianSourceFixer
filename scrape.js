2120ae034a266f0ca1cd553762a7ffb46daab44e
Kethly <57457270+Kethly@users.noreply.github.com>
2021-12-20 16:24:06 -0500

    Update scrape.js

--- scrape.js
+++ scrape.js
@@ -6,7 +6,7 @@ function GetData()  {
   //header('Access-Control-Allow-Origin: *');
   XMLReq.open( "GET", "https://packages.debian.org/");
   
-  XMLReq.setRequestHeader("Referer", "https://packages.debian.org/");
+  XMLReq.setRequestHeader("Origin", "https://packages.debian.org/");
   
 
   XMLReq.onreadystatechange = function() {
