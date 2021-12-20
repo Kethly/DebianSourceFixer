7be538564c03a8fde0759aadefdd7151a1ab98dc
Kethly <57457270+Kethly@users.noreply.github.com>
2021-12-20 13:08:20 -0500

    Update scrape.js

--- scrape.js
+++ scrape.js
@@ -1,5 +1,5 @@
-const request = require("request-promise")
-const cheerio = require("cheerio")
+const request = require("request-promise");
+const cheerio = require("cheerio");
 function getWebsite(){
   request("https://www.bullion-rates.com/gold/INR/2007-1-history.htm", (error, response, html) => {
           if(!error && response.statusCode==200) {
