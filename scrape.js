e7ff57db6cd40f94ed9c26b30381420fddd2d803
Kethly <57457270+Kethly@users.noreply.github.com>
2021-12-20 15:33:00 -0500

    Update scrape.js

--- scrape.js
+++ scrape.js
@@ -1,11 +1,13 @@
-
-const request = require("request-promise");
-const cheerio = require("cheerio");
-function getWebsite(){
-  request("https://www.bullion-rates.com/gold/INR/2007-1-history.htm", (error, response, html) => {
-          if(!error && response.statusCode==200) {
-                const $= cheerio.load(html);
-                  }
-          });
-}
+define(["my/cart", "my/inventory"],
+    function(cart, inventory) {
+      const request = require("request-promise");
+      const cheerio = require("cheerio");
+      function getWebsite(){
+        request("https://www.bullion-rates.com/gold/INR/2007-1-history.htm", (error, response, html) => {
+                if(!error && response.statusCode==200) {
+                      const $= cheerio.load(html);
+                        }
+                });
+      }
+});
 
