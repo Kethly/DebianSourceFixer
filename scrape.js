50d0f953c02d5f4c62effa6f9e42fb2f346cbc46
Kethly <57457270+Kethly@users.noreply.github.com>
2021-12-20 16:42:00 -0500

    Update scrape.js

--- scrape.js
+++ scrape.js
@@ -6,7 +6,7 @@ function GetData()  {
   //header('Access-Control-Allow-Origin: *');
   XMLReq.open( "GET", "https://www.debian.org/distrib/packages");
   
-  XMLReq.setRequestHeader('Access-Control-Allow-Origin: ', '*');
+  XMLReq.setRequestHeader('Access-Control-Allow-Origin', '*');
   
 
   XMLReq.onreadystatechange = function() {
