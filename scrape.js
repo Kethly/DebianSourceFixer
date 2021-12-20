56bc62432263641c49d28ead0f10e47f01549cd2
Kethly <57457270+Kethly@users.noreply.github.com>
2021-12-20 16:18:33 -0500

    Update scrape.js

--- scrape.js
+++ scrape.js
@@ -1,7 +1,11 @@
+var myHeaders = new Headers(); // Currently empty
+myHeaders.append('Content-Type', 'image/jpeg');
+myHeaders.set('Access-Control-Allow-Origin', '*');
 function GetData()  {
   var XMLReq = new XMLHttpRequest();
   //header('Access-Control-Allow-Origin: *');
   XMLReq.open( "GET", "https://packages.debian.org/");
+  
   //XMLReq.setRequestHeader("Access-Control-Allow-Origin", "*");
   
 
