e7637dbeb2bc7a54b76e7ae7d5b247a718aaafd0
Kethly <57457270+Kethly@users.noreply.github.com>
2021-12-20 12:51:35 -0500

    Update scrape.js

--- scrape.js
+++ scrape.js
@@ -1,7 +1,7 @@
 const request = require("request-promise")
 const cheerio = require("cheerio")
 function getWebsite(){
-  request("https://www.bullion-rates.com/gold/INR/2007-1-history.htm, (error, response, html) => {
+  request("https://www.bullion-rates.com/gold/INR/2007-1-history.htm", (error, response, html) => {
           if(!error && response.statusCode==200) {
                 const $= cheerio.load(html);
                   }
