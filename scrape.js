1b7d1184c2ad1a755c3f3871a97417d8a6d65846
Kethly <57457270+Kethly@users.noreply.github.com>
2021-12-20 17:06:37 -0500

    Update scrape.js

--- scrape.js
+++ scrape.js
@@ -1,17 +1,30 @@
 function GetData()  {
-  var XMLReq = new XMLHttpRequest();
-  //header('Access-Control-Allow-Origin: *');
-  XMLReq.open( "GET", "https://www.debian.org/distrib/packages", false);
-  
-  //XMLReq.setRequestHeader('withCredentials', 'true');
-  
-
-  XMLReq.onreadystatechange = function() {
-    if(XMLReq.readyState == 4 && XMLReq.status == 200) {
-      console.log(XMLReq.responseText);
+  fetch("https://world.openfoodfacts.org/category/pastas/1.json")
+  .then(response => {
+    // indicates whether the response is successful (status code 200-299) or not
+    if (!response.ok) {
+      throw new Error(`Request failed with status ${reponse.status}`)
     }
-  }
+    return response.json()
+  })
+  .then(data => {
+    console.log(data.count)
+    console.log(data.products)
+  })
+  .catch(error => console.log(error))
+//   var XMLReq = new XMLHttpRequest();
+//   //header('Access-Control-Allow-Origin: *');
+//   XMLReq.open( "GET", "https://www.debian.org/distrib/packages", false);
+  
+//   //XMLReq.setRequestHeader('withCredentials', 'true');
+  
 
-  XMLReq.send();
+//   XMLReq.onreadystatechange = function() {
+//     if(XMLReq.readyState == 4 && XMLReq.status == 200) {
+//       console.log(XMLReq.responseText);
+//     }
+//   }
+
+//   XMLReq.send();
 }
 
