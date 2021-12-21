b084a52ee143d9d104ec92c4cec196093a85d533
Kethly <57457270+Kethly@users.noreply.github.com>
2021-12-20 21:30:59 -0500

    Update scrape.js

--- scrape.js
+++ scrape.js
@@ -1,28 +1,27 @@
 function GetData()  {
-  fetch("//sources.debian.org/api/search/query", {mode:"cors"})
-  .then(response => {
-    // indicates whether the response is successful (status code 200-299) or not
-    if (!response.ok) {
-      throw new Error(`Request failed with status ${response.status}`)
-    }
-    return response.json();
-  });
-//   var XMLReq = new XMLHttpRequest();
-//   //header('Access-Control-Allow-Origin: *');
-//   XMLReq.open("GET", "https://sources.debian.org/api/search/hello/");
-  
-//   //XMLReq.setRequestHeader('Access-Control-Allow-Origin', 'https://www.google.com/search?q=Details+of+package+bum+debian');
-  
-
-//   XMLReq.onreadystatechange = function() {
-//     XMLReq.response.set('Access-Control-Allow-Origin', '*');
-//     console.log(XMLReq.responseText);
-//     if(XMLReq.readyState == 4 && XMLReq.status == 200) {
-//       console.log(XMLReq.responseText);
+//   fetch("//sources.debian.org/api/search/query", {mode:"cors"})
+//   .then(response => {
+//     // indicates whether the response is successful (status code 200-299) or not
+//     if (!response.ok) {
+//       throw new Error(`Request failed with status ${response.status}`)
 //     }
-//   }
+//     return response.json();
+//   });
+  var XMLReq = new XMLHttpRequest();
+  //header('Access-Control-Allow-Origin: *');
+  XMLReq.open("GET", "https://sources.debian.org/doc/api/#copyright");
+  
+  //XMLReq.setRequestHeader('Access-Control-Allow-Origin', 'https://www.google.com/search?q=Details+of+package+bum+debian');
+  
 
-//   XMLReq.send();
+  XMLReq.onreadystatechange = function() {
+    console.log(XMLReq.responseText);
+    if(XMLReq.readyState == 4 && XMLReq.status == 200) {
+      console.log(XMLReq.responseText);
+    }
+  }
+
+  XMLReq.send();
 }
 
 async function getData() {
