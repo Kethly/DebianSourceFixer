cca15ac4d452ea06c103ef17a416dc12e40219c8
Kethly <57457270+Kethly@users.noreply.github.com>
2021-12-20 21:17:11 -0500

    Update scrape.js

--- scrape.js
+++ scrape.js
@@ -3,7 +3,7 @@ function GetData()  {
   .then(response => {
     // indicates whether the response is successful (status code 200-299) or not
     if (!response.ok) {
-      throw new Error(`Request failed with status ${reponse.status}`)
+      throw new Error(`Request failed with status ${response.status}`)
     }
     return response.json();
   });
