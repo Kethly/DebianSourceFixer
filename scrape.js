dc078945e57641bc6bcd3d8f14153ba92b55f749
Kethly <57457270+Kethly@users.noreply.github.com>
2021-12-20 22:04:43 -0500

    Update scrape.js

--- scrape.js
+++ scrape.js
@@ -28,21 +28,21 @@ async function getData() {
   const response = await fetch('//sources.debian.org/api/search/query/', { 
     headers: {
       "authority": "sources.debian.org",
-      "method": "GET"
-      "path": "/api/search/hello/"
-      "scheme": "https"
-      "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9"
-      "accept-encoding": "gzip, deflate, br"
-      "accept-language": "en-US,en;q=0.9"
-      "cache-control": "max-age=0"
-      "sec-ch-ua": 'Not A;Brand";v="99", "Chromium";v="96", "Google Chrome";v="96"'
-      "sec-ch-ua-mobile": "?0"
-      "sec-ch-ua-platform": "Windows"
-      "sec-fetch-dest": "document"
-      "sec-fetch-mode": "navigate"
-      "sec-fetch-site": "same-origin"
-      "sec-fetch-user": "?1"
-      "upgrade-insecure-requests": "1"
+      "method": "GET",
+      "path": "/api/search/hello/",
+      "scheme": "https",
+      "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+      "accept-encoding": "gzip, deflate, br",
+      "accept-language": "en-US,en;q=0.9",
+      "cache-control": "max-age=0",
+      "sec-ch-ua": 'Not A;Brand";v="99", "Chromium";v="96", "Google Chrome";v="96"',
+      "sec-ch-ua-mobile": "?0",
+      "sec-ch-ua-platform": "Windows",
+      "sec-fetch-dest": "document",
+      "sec-fetch-mode": "navigate",
+      "sec-fetch-site": "same-origin",
+      "sec-fetch-user": "?1",
+      "upgrade-insecure-requests": "1",
       "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36"}});
   const data = await response.json();
   console.log(data);
