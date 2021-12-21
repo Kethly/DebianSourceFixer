f5beaf37175a4331ffc419a83e6f129f69906b5a
Kethly <57457270+Kethly@users.noreply.github.com>
2021-12-20 21:07:00 -0500

    Update scrape.js

--- scrape.js
+++ scrape.js
@@ -1,26 +1,26 @@
 function GetData()  {
-//   fetch("https://www.debian.org/distrib/packages", {Access-Control-Allow-Origin: '*'})
-//   .then(response => {
-//     // indicates whether the response is successful (status code 200-299) or not
-//     if (!response.ok) {
-//       throw new Error(`Request failed with status ${reponse.status}`)
-//     }
-//     return response
-  var XMLReq = new XMLHttpRequest();
-  //header('Access-Control-Allow-Origin: *');
-  XMLReq.open("GET", "https://sources.debian.org/api/search/hello/");
-  
-  //XMLReq.setRequestHeader('Access-Control-Allow-Origin', 'https://www.google.com/search?q=Details+of+package+bum+debian');
-  
-
-  XMLReq.onreadystatechange = function() {
-    XMLReq.response.set('Access-Control-Allow-Origin', '*');
-    console.log(XMLReq.responseText);
-    if(XMLReq.readyState == 4 && XMLReq.status == 200) {
-      console.log(XMLReq.responseText);
+  fetch("https://sources.debian.org/api/search/hello/")
+  .then(response => {
+    // indicates whether the response is successful (status code 200-299) or not
+    if (!response.ok) {
+      throw new Error(`Request failed with status ${reponse.status}`)
     }
-  }
+    return response.json
+//   var XMLReq = new XMLHttpRequest();
+//   //header('Access-Control-Allow-Origin: *');
+//   XMLReq.open("GET", "https://sources.debian.org/api/search/hello/");
+  
+//   //XMLReq.setRequestHeader('Access-Control-Allow-Origin', 'https://www.google.com/search?q=Details+of+package+bum+debian');
+  
 
-  XMLReq.send();
+//   XMLReq.onreadystatechange = function() {
+//     XMLReq.response.set('Access-Control-Allow-Origin', '*');
+//     console.log(XMLReq.responseText);
+//     if(XMLReq.readyState == 4 && XMLReq.status == 200) {
+//       console.log(XMLReq.responseText);
+//     }
+//   }
+
+//   XMLReq.send();
 }
 
