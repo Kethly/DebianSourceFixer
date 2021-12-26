0a9b838b25876a0bf17ccbb9b2c00094ae7166b1
Kethly <57457270+Kethly@users.noreply.github.com>
2021-12-26 18:26:51 -0500

    Update debian_source.js

--- functions/debian_source.js
+++ functions/debian_source.js
@@ -1,16 +1,93 @@
   //const fetch = require("node-fetch");
+  function findByElement(arr, elem){
+  if(!arr){
+    return "";
+  }
+  var found = []; 
+  var a = 0;
+  var b = 0;
+  for (let index = 0; index < arr.length; index++) {
+
+        if(arr[index].indexOf("<" + elem + ">") >= 0 || arr[index].indexOf("<" + elem) >= 0){
+          a = index;
+          //console.log(index);
+        }
+        if(arr[index].indexOf("</" + elem + ">") >= 0){
+          b = index;
+          //console.log(index);
+          found.push(arr.slice(a, b + 1));
+          a = 0;
+          b = 0;
+        }
+ 
+      }
+  return found;
+}
+function getInnerText(arr, initial = ">", end = "<"){ //initial is typically ">", and end is typically "<"
+  var simple = arr.join('');
+  //console.log(simple)
+  var a = 0;
+  var b = 0;
+  var c = "";
+  for (let index = 0; index < simple.length; index++) {
+    if(simple.charAt(index) === initial && a === 0){
+          a = index;
+        }
+        if(simple.charAt(index) === end && a != 0){
+          b = index;
+          c += (simple.slice(a + 1, b));
+          a = 0;
+          b = 0;
+        }
+  }
+  
+  return c;
+}
+
+function loopSearchForPackageType(arr, suite="stable"){
+  if(suite === null){
+    suite = "stable";
+  }
+  if(arr === ""){
+    console.log("nothing was found.  If you're reading this, maybe the package you were looking for isn't here or doesn't exist?  Check your search words again.");
+    return "";
+  }
+  if (arr.length <= 1) { return arr[0]; }
+  for (let index = 0; index < arr.length; index++){
+    if(getInnerText(arr[index]).indexOf("(" + suite + ")") >= 0) {
+      return arr[index];
+    }
+  }
+  return arr[0];
+}
+  
   exports.handler = async (event, context) => {
-  const searchterm = event.queryStringParameters.searchterm;
-  const API_ENDPOINT = '/api/search?keywords=" + searchterm + "&searchon=names&section=all&exact=1';
-  try {
-    const response = await fetch(API_ENDPOINT);
-    const data = await response;
-    return { statusCode: 200, body: data };
-  } catch (error) {
-    console.log(error);
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ error: 'Failed fetching data' }),
-    };
+  const spackage = event.queryStringParameters.package;
+  const ssuite = event.queryStringParameters.suite;
+  var searchterm = spackage;
+  var suite = ssuite;
+  var XMLReq = new XMLHttpRequest();
+  var htmlArray = [];
+  XMLReq.open("GET", "/api/search?keywords=" + searchterm + "&searchon=names&section=all&exact=1");
+  XMLReq.onreadystatechange = function() {
+    if(XMLReq.readyState == 4 && XMLReq.status == 200) {
+      //console.log(XMLReq.responseText);
+      htmlArray = XMLReq.responseText.split("\n")
+      for (let index = 0; index < htmlArray.length; index++) {
+        htmlArray[index] = htmlArray[index].trim();
+        
+      }
+      htmlArray = htmlArray.filter(n => n);
+      var searchResult = getInnerText(loopSearchForPackageType(findByElement(findByElement(htmlArray, "ul")[1], "li"), suite)).split(" ")[0] + "/" + searchterm;
+      console.log(searchResult);
+      return { statusCode: 200, body: searchResult };
+     // var test = window.open("/test.html");
+      //test.document.write("<html><head><title>MyTitle</title></head><body>test</body></html>");
+      
+      //return searchResult;
+    }
+  }
+
+  XMLReq.send();
   }
 };
