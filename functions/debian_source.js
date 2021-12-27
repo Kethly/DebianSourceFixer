54cf020c1eef1974505a18a7f25e827f1578017a
Kethly <57457270+Kethly@users.noreply.github.com>
2021-12-27 00:59:28 -0500

    Update debian_source.js

--- functions/debian_source.js
+++ functions/debian_source.js
@@ -103,7 +103,8 @@ var htmlArray = data.split("\n"); //JSON.stringify(data).split("\n");
   var packageDownload = await fetch("https://packages.debian.org/" + searchResult);
   data = await packageDownload.text();
   htmlArray = clean_up_html(data);
-  searchResult += "/" + getInnerText(loopSearchForArchType(findByElement(htmlArray, 'th'), arch)) + "/download";//findByElement(htmlArray, 'div id=\"pdownload\"', 'div');
+  searchResult = findByElement(htmlArray, 'th')
+  //searchResult += "/" + getInnerText(loopSearchForArchType(findByElement(htmlArray, 'th'), arch)) + "/download";//findByElement(htmlArray, 'div id=\"pdownload\"', 'div');
 //   var mirrorlinks = await fetch("https://packages.debian.org/" + searchResult);
 //   data = await mirrorlinks.text();
 //   htmlArray = clean_up_html(data);
