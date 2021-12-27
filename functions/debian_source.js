69f1314dde403bbe009af1ab858b08b3d54f6c7a
Kethly <57457270+Kethly@users.noreply.github.com>
2021-12-26 20:43:01 -0500

    Update debian_source.js

--- functions/debian_source.js
+++ functions/debian_source.js
@@ -83,7 +83,7 @@ return one + two;
   }
   htmlArray = htmlArray.filter(n => n);
   var searchResult = getInnerText(loopSearchForPackageType(findByElement(findByElement(htmlArray, "ul")[1], "li"), suite)).split(" ")[0] + "/" + searchterm;
-  return { statusCode: 200, body: JSON.stringify(htmlArray), };
+  return { statusCode: 200, body: JSON.stringify(searchResult), };
   var XMLReq = new XMLHttpRequest();
   var htmlArray = [];
   XMLReq.open("GET", "/api/search?keywords=" + searchterm + "&searchon=names&section=all&exact=1");
